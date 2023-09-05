420383551080cc5a624f9753c17a7afded5f1e67
Angel Velasquez <avelasquezn@gmail.com>
2023-09-05 09:25:28 -0500

    feat(main-content): added template code for article cards.

--- src/app/news/components/main-content/main-content.component.ts
+++ src/app/news/components/main-content/main-content.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-main-content',
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./main-content.component.css']
 })
 export class MainContentComponent {
+  title = 'CatchUp';
+
+  @Input() articles: Array<any> = [];
 
 }
